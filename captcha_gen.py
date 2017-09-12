de64005e38f0a10604b277d6b4361c8b2e899d92
Radim Keske <radim.keske@gmail.com>
2017-09-12 15:19:26 +0200

    Create captcha_gen.py

---
+++ captcha_gen.py
@@ -0,0 +1,50 @@
+from PIL import ImageFont, Image, ImageDraw
+import random
+import string
+import os
+
+
+class Captcha:
+    def __init__(self):
+        self.key = self.random_key()
+        self.font = ImageFont.truetype("MotivaSans-Thin.otf", 23)
+        self.size = (120, 35)
+        self.background = (245, 245, 245)
+        self.circle_color = (180, 180, 180)
+        self.rotate = [angle for angle in range(-2, 2) if angle != 0]
+        self.x_off = 10
+        self.y_off = 3
+        self.char_color = [(150, 150, 150), (92, 92, 92)]
+
+    def random_key(self):
+        key = ''
+        for letter in range(6):
+            key += random.choice(string.ascii_lowercase + string.digits)
+        return key
+
+    def create_image(self):
+        image1 = Image.new("RGB", self.size, self.background)
+        draw = ImageDraw.Draw(image1)
+
+        for i in range(4):
+            x, y, r = (random.randint(0, 120), random.randint(0, 35), random.randint(3, 20))
+            draw.ellipse((x - r, y - r, x + r, y + r), outline=self.circle_color)
+
+        for char in self.key:
+            image2 = Image.new("RGBA", self.size)
+            draw2 = ImageDraw.Draw(image2)
+            draw2.text((self.x_off, self.y_off), text=char, font=self.font, fill=random.choice(self.char_color))
+            image2 = image2.rotate(random.choice(self.rotate), expand=1)
+            image1.paste(image2, (0, 0), image2)
+            self.x_off += 16
+
+        path = "./data/"
+
+        if os.path.exists(path) is False:
+            os.mkdir(path)
+
+        image1.save(path + self.key + ".png")
+
+
+if __name__ == "__main__":
+    [Captcha().create_image() for x in range(100)]  # number of captchas to be created
